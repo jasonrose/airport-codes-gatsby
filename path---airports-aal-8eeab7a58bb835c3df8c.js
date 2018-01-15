6475cb06974d5bd3e53f7aa8fa81bca902c0b269
Jason Rose <jasoncrose@gmail.com>
2018-01-14 17:54:00 -0800

    Updates

--- path---airports-aal-8eeab7a58bb835c3df8c.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([44039538972220],{328:function(r,t){r.exports={data:{airportsJson:{city:"Aalborg",city2:"Nørresundby",country:"Denmark",description:"Located in the city of Nørresundby, Aalborg Airport is the third largest in Denmark and serves the city of *Aal*borg.",id:"aal",imageCredit:"Greg Myers",imageCreditLink:"https://www.flickr.com/photos/greg_myers/",name:"Aalborg Lufthavn",nameEnglish:"Aalborg Airport"}},pathContext:{slug:"/airports/aal/"}}}});
-//# sourceMappingURL=path---airports-aal-8eeab7a58bb835c3df8c.js.map
